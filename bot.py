f0d033851111fefcc90e54196beb438ffc59198a
Harshita <jyotishmoigogoigogoi@gmail.com>
2026-05-23 22:16:25 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -89,7 +89,7 @@ class Bot(Client):
         try:
             db_channel = await self.get_chat(CHANNEL_ID)
             self.db_channel = db_channel
-            test = await self.send_message(chat_id = db_channel.id, text = "@YoriFederation file share test")
+            test = await self.send_message(chat_id = db_channel.id, text = "@YoriFederation file share bot")
             await test.delete()
         except Exception as e:
             self.LOGGER(__name__).warning(e)
